2ae4e62e15982feb528791152632563f7dcd53ee
rodrigo-medeiros <rodrigo.mangue@gmail.com>
2013-12-14 18:13:38 -0200

    Added the solution to the 'Array.diff' kata.

---
+++ javascript-fundamentals/array-diff.js
@@ -0,0 +1,14 @@
+// Your goal in this kata is to implement an array_diff function, which subtracts one array from another.
+
+// It should remove all values from array a, which are present in array b.
+
+// array_diff([1,2], [1]) // [2]
+// If a value is present in array b, all of its occurrences must be removed from array a.
+
+// array_diff([1,2,2], [2]) // [1]
+
+function array_diff(a, b) {
+  return a.filter(function (item) {
+    return b.indexOf(item) < 0;
+  });
+}
