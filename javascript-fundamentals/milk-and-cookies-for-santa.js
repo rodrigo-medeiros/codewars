72d9d1890953a34eb1dc0a93ddb133dc38f06937
rodrigo-medeiros <rodrigo.mangue@gmail.com>
2013-12-17 23:57:21 -0200

    Added the solution to the 'Milk and cookies for santa' kata.

---
+++ javascript-fundamentals/milk-and-cookies-for-santa.js
@@ -0,0 +1,21 @@
+// Happy Holidays fellow Code Warriors!
+
+// It's almost Christmas Eve, so we need to prepare some milk and cookies for Santa! Wait..when exactly did we need to do that?
+
+// Time for Milk and Cookies
+
+// Write a function timeForMilkAndCookies (time_for_milk_and_cookies in Ruby) that accepts a Date object,
+// and returns true if it's Christmas Eve (December 24th), false otherwise.
+// Keep in mind Javascript's Date month is 0 based, while Ruby's Date month isn't.
+
+// Examples
+
+// Javascript/CoffeeScript:
+
+// timeForMilkAndCookies( new Date( 2013, 11, 24 ) ) // December 24, 2013 => returns true
+// timeForMilkAndCookies( new Date( 2013, 0, 23 ) ) // January 23, 2013 => returns false
+// timeForMilkAndCookies( new Date( 3000, 11, 24 ) ) //  Decemb
+
+function timeForMilkAndCookies (date) {
+  return date.getDate() === 24 && date.getMonth() === 11;
+}
