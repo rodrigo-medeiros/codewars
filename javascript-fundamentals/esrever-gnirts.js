2969a59462235ae7da2212fbc3b3ddcc517db2dd
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-01-02 01:30:41 -0200

    Added the solution to the 'esreveR gnirtS' kata.

---
+++ javascript-fundamentals/esrever-gnirts.js
@@ -0,0 +1,8 @@
+// Create a function called reverse for the String prototype that will allow the following functionality:
+
+// "String".reverse();// => returns "gnirtS"
+// "Super awesome string".reverse();// => returns "gnirts emosewa repuS"
+
+String.prototype.reverse = function () {
+  return this.split('').reverse().join('');
+};
