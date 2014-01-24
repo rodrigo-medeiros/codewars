425231d4f2ef0f7fa21bb855e956bbbbce5c987d
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-01-23 22:44:06 -0200

    Added some more tests.

--- spec/weird-string-case-spec.js
+++ spec/weird-string-case-spec.js
@@ -2,13 +2,21 @@ describe("'Weird string case' kata tests", function () {
 
   var toWeirdCase = require("../weird-string-case");
 
-  it("should return 'StRiNg'", function () {
+  it("should return the correct value for a single word", function () {
     var result = toWeirdCase("String");
     expect(result).toBe("StRiNg");
   });
 
-  it("should return 'WeIrD StRiNg CaSe'", function () {
-    var result = toWeirdCase("Weird string case");
-    expect(result).toBe("WeIrD StRiNg CaSe");
+  it("should return the correct value to multiple words, with one ' ' in between", function () {
+    var sentences = {
+      "This is a test": "ThIs Is A TeSt",
+      "Should return the correct value": "ShOuLd ReTuRn ThE CoRrEcT VaLuE",
+      "Multiple words": "MuLtIpLe WoRdS"
+    };
+
+    for (var key in sentences) {
+      var result = toWeirdCase(key);
+      expect(result).toBe(sentences[key]);
+    }
   });
 });
